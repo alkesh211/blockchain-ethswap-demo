bd60951fb0a84d8308f5e5e3769bec35fa8dcbde
Alkesh <alkesh@vibrant-info.com>
2022-05-19 13:09:18 +0530

    Add icons and Update layout

--- src/components/App.js
+++ src/components/App.js
@@ -84,7 +84,7 @@ class App extends Component {
         <Navbar account={this.state.account} />
         <div className="container-fluid mt-5">
           <div className="row">
-            <main role="main" className="col-lg-12 d-flex text-center">
+            <main role="main" className="col-lg-12 ml-auto mr-auto" style={{ maxWidth:'600px' }}>
               <div className="content mr-auto ml-auto">
                 <a
                   href="http://www.dappuniversity.com/bootcamp"
